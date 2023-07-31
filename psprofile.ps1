4f0528f86d53c890b88fed2bf80abe0d4fc3f20b
Shafin Ahammed <shafinahammed000@gmail.com>
2023-07-31 15:29:38 +0530

    Changes

--- psprofile.ps1
+++ psprofile.ps1
@@ -17,3 +17,28 @@ Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 #Allias
 Set-Alias np notepad
 Set-Alias c cls
+
+function My-Ping {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$TargetHost
+    )
+
+    # Perform the ping command with specific parameters
+    ping $TargetHost -l 2000 -t
+}
+
+# Create an alias for My-Ping function with the name "mping"
+New-Alias -Name "p" -Value "My-Ping"
+
+
+
+function PublicIp 
+{
+	$ip = Invoke-WebRequest -Uri "https://ifconfig.me" -UseBasicParsing | Select-Object -ExpandProperty Content
+Write-Host $ip
+
+}
+
+# Create an alias for My-Ping function with the name "mping"
+New-Alias -Name "myip" -Value "PublicIp"
